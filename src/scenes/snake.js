a109edef239d9410cc288980ea5c82427537794a
bafiam <stevebafiam@gmail.com>
2020-05-25 16:40:16 +0300

    create snake instance and set properties

--- src/scenes/snake.js
+++ src/scenes/snake.js
@@ -1,13 +1,45 @@
 import Phaser from 'phaser/dist/phaser';
 
+
+var snake;
+var cursors;
+
+var UP = 0;
+var DOWN = 1;
+var LEFT = 2;
+var RIGHT = 3;
+
 class MainScene extends Phaser.Scene {
+  
   preload(){
     this.load.image('snake','assets/body.png');
 
   }
   create() {
-    this.add.text(100, 100, 'Hello Stephen!', { fill: '#0f0' });
-    this.add.image(200, 200, 'snake');
+    const Createsnake = new Phaser.Class({
+
+      initialize:
+
+      function Createsnake (scene, x, y)
+      {
+          this.headPosition = new Phaser.Geom.Point(x, y);
+
+          this.snakeBody = scene.add.group();
+
+          this.snakeHead = this.snakeBody.create(x * 16, y * 16, 'snake');
+          this.snakeHead.setOrigin(0);
+
+          this.alive = true;
+
+          this.snakeSpeed = 100;
+
+          this.moveTime = 0;
+
+          this.heading = RIGHT;
+          this.direction = RIGHT;
+      }
+    });
+    snake = new Createsnake(this, 8, 8);
   }
   update (time, delta){
     
