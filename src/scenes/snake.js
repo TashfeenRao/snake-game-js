847e0558483f3f54e76cf7035f0871420d8b5df9
bafiam <stevebafiam@gmail.com>
2020-05-25 17:39:44 +0300

    allow screen warping if it goes off on one side, appear on the other

--- src/scenes/snake.js
+++ src/scenes/snake.js
@@ -37,7 +37,47 @@ class MainScene extends Phaser.Scene {
 
           this.heading = RIGHT;
           this.direction = RIGHT;
-      }
+      },
+      
+      move: function (time)
+      {
+          switch (this.heading)
+          {
+              case LEFT:
+                  this.headPosition.x = Phaser.Math.Wrap(this.headPosition.x - 1, 0, 40);
+                  break;
+
+              case RIGHT:
+                  this.headPosition.x = Phaser.Math.Wrap(this.headPosition.x + 1, 0, 40);
+                  break;
+
+              case UP:
+                  this.headPosition.y = Phaser.Math.Wrap(this.headPosition.y - 1, 0, 30);
+                  break;
+
+              case DOWN:
+                  this.headPosition.y = Phaser.Math.Wrap(this.headPosition.y + 1, 0, 30);
+                  break;
+          }
+
+          this.direction = this.heading;
+
+          //  Update the body segments
+          Phaser.Actions.ShiftPosition(this.snakeBody.getChildren(), this.headPosition.x * 16, this.headPosition.y * 16, 1);
+
+          //  Update the timer ready for the next movement
+          this.moveTime = time + this.speed;
+
+          return true;
+      },
+
+      update: function (time)
+        {
+            if (time >= this.moveTime)
+            {
+                return this.move(time);
+            }
+        },
     });
     snake = new Createsnake(this, 8, 8);
   }
