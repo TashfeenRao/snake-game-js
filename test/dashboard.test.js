9853192a58f5b39a8dff48990d9b44283a164db6
bafiam <stevebafiam@gmail.com>
2020-05-28 18:36:22 +0300

    add api testing

---
+++ test/dashboard.test.js
@@ -0,0 +1,16 @@
+import { postUserScore, getAllUserScores } from "../src/utils/dashboard";
+
+
+test('should successfully create the player and score given name and score', async () => {
+  const data = await postUserScore('steve-test', 100);
+  expect(data.data.result).toBe('Leaderboard score created correctly.');
+  expect(data.status).toBe(201);
+  expect(data.statusText).toBe('Created');
+});
+
+test('should successfully return all players and their scores', async () => {
+  const result = await getAllUserScores();
+  expect(typeof result).toBe('object');
+  expect(result.status).toBe(200);
+  expect(result.statusText).toBe('OK');
+});
